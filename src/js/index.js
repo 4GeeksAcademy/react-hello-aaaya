7a6f2bfc9a3e21b61aec27b198f9c95649a32248
Ajaya-magar <156473678+Ajaya-magar@users.noreply.github.com>
2024-08-15 11:48:06 +0000

    First commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,8 @@
-//import react into the bundle
+
+
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import TrafficLight from "./TrafficLight";
+import "./TrafficLight.css";
 
-// include your styles into the webpack bundle
-import "./styles.css";
-
-//import your own components
-import Home from "./component/home.jsx";
-
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
-
+ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
